e888b1534d225ca8e3479792936dc5aa388e5bde
lefteris797 <lefteris797@gmail.com>
2026-01-26 01:23:05 +0200

    feat:add main for creating instances

--- src/gr/aueb/cf/cf9/ch11/Main.java
+++ src/gr/aueb/cf/cf9/ch11/Main.java
@@ -9,11 +9,31 @@ package gr.aueb.cf.cf9.ch11;
  */
 public class Main {
     public static void main(String[] args) {
+
+        Costumer customer = new Costumer(1L, "Gainnis", "Dimitriou", "5464", "695646216", "trikala",
+                "trikalwn", "Thesalias", "Trikala", "Solwnos", "3", "lefteris@gmail.com");
+
+
+        Product mik = new Product(1L,"Milko","Chocolate milk", 1,20, true);
+
+
+
+
+
+
+
+
+
+
         Student student = new Student();
         Student alice = new Student();
         Student bob = new Student();
         Student makis = new Student(3,"Makis","Kapetis");
         makis.setLastname("Xrisostomos");
+
+        User andreas22 = new User(1L,"Andreas", "Papadopoulos", "andreas22", "aasa1", true);
+        System.out.println(andreas22.getFirstname());
+
         alice.setId(1);
         alice.setFirstname("Alice");
         alice.setLastname("Wonderland");
