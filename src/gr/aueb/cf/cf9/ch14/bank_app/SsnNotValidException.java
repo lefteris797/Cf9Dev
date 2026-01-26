945ecdd25e1ed87663b29d055e6dee2ed25b22ac
lefteris797 <lefteris797@gmail.com>
2026-01-27 00:11:27 +0200

    feat: correcting

--- src/gr/aueb/cf/cf9/ch14/bank_app/SsnNotValidException.java
+++ src/gr/aueb/cf/cf9/ch14/bank_app/SsnNotValidException.java
@@ -2,6 +2,6 @@ package gr.aueb.cf.cf9.ch14.bank_app;
 
 public class SsnNotValidException extends Exception{
     public SsnNotValidException(String message){
-        short(message);
+        super(message);
     }
 }
