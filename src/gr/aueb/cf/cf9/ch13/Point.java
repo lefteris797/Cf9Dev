86162f59a98eff9e4f015be9c77126b4ebff7b3f
lefteris797 <lefteris797@gmail.com>
2026-01-26 03:45:32 +0200

    feat:add point

---
+++ src/gr/aueb/cf/cf9/ch13/Point.java
@@ -0,0 +1,29 @@
+package gr.aueb.cf.cf9.ch13;
+
+import java.util.Random;
+
+public class Point {
+   private int x;
+   private int y;
+
+   public Point(){}
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Static factory method
+     * @return
+     */
+    public static Point getInstance(){
+        return new Point();
+    }
+
+    public static Point getRandomPoint(){
+        final int BOUND = 101;
+        Random random = new Random(System.nanoTime());
+        return new Point(random.nextInt(BOUND), random.nextInt(BOUND));
+    }
+}
