d5f5e9937ec4ab37c219a1cfd7de22c08031ffa0
lefteris797 <lefteris797@gmail.com>
2026-01-26 03:08:20 +0200

    feat:add account with both data and public API

---
+++ src/gr/aueb/cf/cf9/ch12/Account.java
@@ -0,0 +1,124 @@
+package gr.aueb.cf.cf9.ch12;
+
+import java.sql.SQLOutput;
+
+public class Account {
+    private long id;
+    private String iban;
+    private String firstname;
+    private String lastname;
+    private String ssn;
+    private double balance;
+
+    public Account{
+    }
+
+    public Account(long id, String iban, String firstname, String lastname, String ssn, double balance) {
+        this.id = id;
+        this.iban = iban;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.ssn = ssn;
+        this.balance = balance;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    //Public API
+
+    /**
+     * Deposits the specified amount of money into the account
+     * @param amount the amount of money to deposit
+     * @throws Exception if the amount of money is negative
+     */
+    public void deposit(double amount) throws Exception{
+        try{
+            if (amount < 0) throw new Exception("The amount should be positive");
+            balance += amount;
+        }
+        catch (Exception e){
+            System.err.println("Negative amount = " + amount + "is not allowed");
+            throw e;
+        }
+    }
+
+    /**
+     *  Withdraws the specified amount of money
+     * @param amount the amount of money to withdraw
+     * @param ssn the SSN of the account
+     * @throws Exception if the amount is negative or there anot sufficient funds or the ssn dont match the account's ssn
+     */
+    public void withdraw(double amount, String ssn) throws Exception {
+        try {
+            if (amount < 0) throw new Exception("The amount should be positive");
+            if (amount > balance) throw new Exception("Insufficient funds");
+            if (!isSsnValid(ssn)) throw new Exception("The ssn does not match");
+            balance -= amount;
+        } catch (Exception e) {
+            System.err.println("Withdrawal failed" + "\n" + e.getMessage());
+            throw e
+        }
+    }
+        /**
+         * Returns the current balance of the account
+         */
+        public double getAccountBalance(){
+            return getBalance();
+        }
+        /**
+         * returns the state of the account as a string
+         */
+        public String accountToString(){
+        return "Account Id:" + id + "IBAN" + iban + "Firstname:" + firstname + "Last name" + lastname + "SSN" + ssn + "Balance" + balance;
+        }
+        private boolean isSsnValid(String ssn){
+            return this.ssn.equals(ssn);
+        }
+}
+
+
